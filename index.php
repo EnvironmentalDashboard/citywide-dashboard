af74e03ae29c515d1da270b2cb75c66450346854
Hitesh Jangid <hiteshjangid19@gmail.com>
2024-11-18 17:20:30 +0530

    refactor index.php and kiosk.php to use cwd-files base URL for dashboard, and replace inline styles with external style.css

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <title>CWD</title>
-  <style>
-  	html, body {width: 100%; height: 100%;}
-  	body {margin:0px;padding:0px;background:#000;}
-  	#dashboard {width:100%;}
-  </style>
+  <link rel="stylesheet" href="./style.css">
 </head>
+
 <body>
-<object id="dashboard" type="image/svg+xml" data="//<?php echo $_SERVER['HTTP_HOST'] . '/' . explode('/', $_SERVER['REQUEST_URI'])[1]; ?>/dashboard.php?<?php echo http_build_query($_GET); ?>"></object>
+  <?php
+  $baseURL  = "cwd-files";
+  $queryParams = http_build_query($_GET);
+  $cwdURL = $_SERVER['HTTP_HOST'] . "/$baseURL/dashboard.php?$queryParams"
+  ?>
+  <object id="dashboard" type="image/svg+xml" data="//<?php echo $cwdURL ?>"></object>
 </body>
+
 </html>
